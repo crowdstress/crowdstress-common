0152afb46a2ec621248f3deaa26063eea36bc88b
xyz27900 <xyz27900@gmail.com>
2021-02-20 00:43:55 +0300

    Add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-pub mod primitives;
-pub mod room;
-pub mod drawing_object;
-pub mod exit;
-pub mod vector;
-pub mod wall;
-pub mod geometry;
+mod drawing_object;
+mod exit;
+mod geometry;
+mod primitives;
+mod room;
+mod vector;
+mod wall;
 
 #[macro_use]
 extern crate serde_derive;
+
+pub mod prelude {
+    pub use crate::{
+        drawing_object::DrawingObject,
+        exit::Exit,
+        geometry,
+        primitives::{Point, Polygon, Section},
+        room::Room,
+        vector::Vector,
+        wall::Wall,
+    };
+}
